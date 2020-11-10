a568e89084e646dddfdd832399a693a0aa9e91e0
RhisiartGwyn <74233823+RhisiartGwyn@users.noreply.github.com>
2020-11-10 18:43:55 +0100

    Update newton_temp.m

--- newton_temp.m
+++ newton_temp.m
@@ -1,13 +1,22 @@
 syms f(x) 
-f(x) = x^2-2;
+f(x) = sin(x);
 df = diff(f);
 format long
-x0 = 1;
-N=5;
-x= x0;
+x0 = 2;
+N = 30;
+x = x0;
+k = 0;
+tol = 1e-15;
+err = 1;
 
-for k = 1:N
+while k < N && err > tol
     
-    x = x-(f(x)/df(x));
-    double (x)
+    x1 = x-(f(x)/df(x));
+    double (x1)
+    err = abs(double(x) - double(x1))
+    x = x1;
+    k = k +1;
+    
 end
+
+err < tol
